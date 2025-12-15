f3d84e3c223c33fca68a509eef5f3a92fb4bbeeb
MalteFries <57703515+MalteFries@users.noreply.github.com>
2025-12-15 23:47:24 +0100

    chore: bump version to 0.9.0

--- jetbrains-plugin/build.gradle.kts
+++ jetbrains-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.kyco"
-version = "0.8.8"
+version = "0.9.0"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ kotlin {
 intellijPlatform {
     pluginConfiguration {
         name = "Kyco"
-        version = "0.8.8"
+        version = "0.9.0"
         ideaVersion {
             sinceBuild = "241"
             untilBuild = provider { null }
