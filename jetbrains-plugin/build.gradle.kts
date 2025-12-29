dc14f2f94e6ade49c08179b359adb384b36fe746
MalteFries <57703515+MalteFries@users.noreply.github.com>
2025-12-29 21:17:08 +0100

    release: v0.10.1

--- jetbrains-plugin/build.gradle.kts
+++ jetbrains-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.kyco"
-version = "0.9.35"
+version = "0.10.1"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ kotlin {
 intellijPlatform {
     pluginConfiguration {
         name = "Kyco"
-        version = "0.9.35"
+        version = "0.10.1"
         ideaVersion {
             sinceBuild = "241"
             untilBuild = provider { null }
